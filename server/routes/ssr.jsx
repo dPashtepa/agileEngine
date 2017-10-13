2567e6f512af7bf27bc558d2e0e007cfa4399803
Dmytro Pashtepa <dp@vi.ai>
2017-10-13 13:53:49 +0300

    changes in configuration

--- server/routes/ssr.jsx
+++ server/routes/ssr.jsx
@@ -5,7 +5,6 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { StaticRouter } from 'react-router';
 import reducers from '../../client/src/reducers/index';
-import { LIST_ACTIONS } from '../../client/src/consts/action_types';
 import App from '../../client/src/app';
 
 const router = express.Router();
@@ -15,32 +14,6 @@ router.get('/', (req, res) => {
     http://redux.js.org/docs/recipes/ServerRendering.html
   */
   const store = createStore(reducers);
-
-  /*
-      We can dispatch actions from server side as well. This can be very useful if you want
-      to inject some initial data into the app. For example, if you have some articles that
-      you have fetched from database and you want to load immediately after the user has loaded
-      the webpage, you can do so in here.
-
-      Here we are inject an list item into our app. Normally once the user has loaded the webpage
-      we would make a request to the server and get the latest item list. But in the server we have
-      instant connection to a database (for example, if you have a mongoDB or MySQL database installed
-      in the server which contains all you items). So you can quickly fetch and inject it into the webpage.
-
-      This will help SEO as well. If you load the webpage and make a request to the server to get all the
-      latest items/articles, by the time Google Search Engine may not see all the updated items/articles.
-
-      But if you inject the latest items/articles before it reaches the user, the Search Engine will see the
-      item/article immediately.
-       */
-  store.dispatch({
-    type: LIST_ACTIONS.ITEM_ADD,
-    item: {
-      name: 'middleware',
-      description: `Redux middleware solves different problems than Express or Koa middleware, but in a conceptually similar way.
-      It provides a third-party extension point between dispatching an action, and the moment it reaches the reducer.`,
-    },
-  });
 
   const context = {};
 
